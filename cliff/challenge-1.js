5e6fc79d0b58bb0529d6ee69cd2e6fae9deed39c
Clifford Morin <cemorin21@gmail.com>
2022-04-26 21:13:26 -0400

    added challenge

---
+++ cliff/challenge-1.js
@@ -0,0 +1,4 @@
+//Today is arrays and array methods. Look up multiple ways to initiate an array,
+//look up a few array methods, and pick one method and create a function that does
+//the exact same thing as the method. (Like pop() => function that takes an array
+//and returns the array without the last element
