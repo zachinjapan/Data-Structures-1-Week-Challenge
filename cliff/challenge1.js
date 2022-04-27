ee444eb4b0198df1635198db3ee09be88eb7ceb4
Clifford Morin <cemorin21@gmail.com>
2022-04-26 22:42:57 -0400

    added comments

--- cliff/challenge1.js
+++ cliff/challenge1.js
@@ -3,12 +3,15 @@
 //the exact same thing as the method. (Like pop() => function that takes an array
 //and returns the array without the last element
 
+//initiate an array
 let arr = [1, 2, 3, 4, 5, 10, 7];
 
 const popSlice = (arr) => {
+  //if the array has one or more element, pop the last element
   if (arr.length) {
     return arr.slice(0, arr.length - 1);
   }
+  //if the array is empty, return an empty array
   return "array is empty!";
 };
 
