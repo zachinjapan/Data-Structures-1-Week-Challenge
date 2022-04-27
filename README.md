5a9e3bc77c3ce09ed8ca6c24de90aa7d5c9ef5c4
Zach Stone <zachinjapan888@gmail.com>
2022-04-27 08:56:41 -0400

    completed challenge 2

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ how to run the challenges:
 node <foldername>/<javascriptfilename>
 
 1. Arrays. Build an array in different ways. Research different array methods and recreate an array method in a function.
+
+2. Singely Linked Lists. Build a singely linked list in different ways. Research different linked list methods and recreate a linked list with the following methods:
+   add
+   removeHead
+   removeTail
+   contains
+   findIf(callback)
+   removeDuplicates
+   print
+   printLength
---
+++ zach/challenge2.js
@@ -0,0 +1,123 @@
+class SLL {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  add(val) {
+    let currNode = new Node(val);
+
+    if (!this.head) {
+      this.head = currNode;
+      this.tail = currNode;
+    } else {
+      this.tail.next = currNode;
+      this.tail = currNode;
+    }
+    this.length++;
+  }
+
+  removeHead() {
+    if (!this.head) return null;
+
+    let removedHead = this.head;
+    this.head = this.head.next;
+    this.length--;
+    return removedHead;
+  }
+
+  removeTail() {
+    if (!this.head) return null;
+
+    let current = this.head;
+
+    while (current.next.next) {
+      current = current.next;
+    }
+
+    let removedTail = current.next;
+    this.tail = current;
+    current.next = null;
+    this.length--;
+    return removedTail;
+  }
+
+  contains(value) {
+    let curr = this.head;
+    while (curr) {
+      if (curr.value === value) return true;
+      curr = curr.next;
+    }
+    return false;
+  }
+
+  findIf(callback) {
+    let curr = this.head;
+    let found = [];
+    while (curr) {
+      if (callback(curr.value)) {
+        found.push(curr.value);
+      }
+      curr = curr.next;
+    }
+
+    if (found.length === 0) return null;
+    return found;
+  }
+
+  removeDuplicates() {
+    let curr = this.head;
+    let valueList = {};
+    while (curr !== null && curr.next !== null) {
+      if (valueList[curr.next.value]) {
+        curr.next = curr.next.next;
+      } else {
+        valueList[curr.next.value] = true;
+        curr = curr.next;
+      }
+    }
+
+    console.log(this.print());
+  }
+
+  print() {
+    let curr = this.head;
+    let str = "";
+    while (curr) {
+      str += curr.value + " ";
+      curr = curr.next;
+    }
+    return str;
+  }
+
+  printLength() {
+    return this.length;
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+let myList = new SLL();
+myList.add(2323);
+myList.add(1);
+myList.add(2);
+myList.add(3);
+myList.add(4);
+myList.add(4);
+myList.add(2);
+myList.add(3234234);
+myList.removeTail();
+myList.removeHead();
+console.log(myList.contains(3));
+// find even numbers
+console.log(myList.findIf((val) => val % 2 === 0));
+console.log(myList.print());
+myList.removeDuplicates();
+console.log(myList.print());
+console.log(myList);
