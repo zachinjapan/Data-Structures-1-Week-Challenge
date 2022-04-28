5375fc5b488cf8b12f0c85130c11794aa65b0a8a
Clifford Morin <cemorin21@gmail.com>
2022-04-28 01:46:38 -0400

    added challenge 2

---
+++ cliff/challenge2.js
@@ -0,0 +1,7 @@
+// Today's challenge research linked lists and create a singly linked list that has the following methods. AddToTail() addToHead()
+// DeleteFromTail()
+// ReplaceAll(oldvalue, newvalue)
+// FindIf(function that returns true or false)* findIf returns an array of values that the passed in function returns as true.
+// PrintList()
+// PrintListLength()
+// Remove Duplicates()
